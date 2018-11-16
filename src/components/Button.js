fd1572bc4fa67139b78ec7b7f3fc53b04d878c86
kombans <alibhaiasj@gmail.com>
2018-11-16 14:22:39 +0530

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    const className = this.props.wide ? "wide" : "";
+    return (
+      <button className={className} onClick={this.props.onClick}>
+        {this.props.text}
+      </button>
+    );
+  }
+}
+
+export default Button;
